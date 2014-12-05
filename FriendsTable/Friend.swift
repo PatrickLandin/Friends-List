d3d4f00401830e54a5893175f04d61c3e232c8fa
Patrick Landin <patricklandin@gmail.com>
2014-12-05 14:24:12 -0800

    first commit

---
+++ FriendsTable/Friend.swift
@@ -0,0 +1,20 @@
+//
+//  Friend.swift
+//  FriendsTable
+//
+//  Created by Patrick Landin on 12/5/14.
+//  Copyright (c) 2014 Patrick Landin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Friend {
+    
+    var name : String
+    var image : UIImage?
+
+    init () {
+        self.name = "Patrick"
+    }
+}
---
+++ FriendsTable/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  FriendsTable
+//
+//  Created by Patrick Landin on 12/5/14.
+//  Copyright (c) 2014 Patrick Landin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var friendName = [Friend]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var tina = Friend()
+        tina.name = "Tina Pulliam"
+        var ryan = Friend()
+        ryan.name = "Ryan Christensen"
+        
+        self.friendName = [tina, ryan]
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.friendName.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FRIEND_CELL", forIndexPath: indexPath) as UITableViewCell
+        
+        var friendToDisplay = self.friendName[indexPath.row]
+        cell.textLabel.text = friendToDisplay.name
+        
+        return cell
+    }
+
+}
+
